089ec7ca77a69c0f52f06acc915679220881cfa6
Sxnty-19 <santymaciasmarquez21@gmail.com>
2026-02-26 17:22:42 -0500

    Add services

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './pages/inicio/inicio';
+import { NoEncontrado } from './pages/no-encontrado/no-encontrado';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+import { Principal } from './pages/principal/principal';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Inicio },
+    { path: '**', component: NoEncontrado },
+    { path: 'login', component: Login },
+    { path: 'register', component: Register },
+    { path: 'principal', component: Principal }
+];
