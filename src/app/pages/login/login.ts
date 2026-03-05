71d32e69fc5f615ab3453e7507ff7caf43a1095c
Sxnty-19 <santymaciasmarquez21@gmail.com>
2026-03-05 11:42:06 -0500

    Add Login

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,47 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  username: string = '';
+  password: string = '';
+  inlineError: string = '';
 
+  isLoading: boolean = false;
+  passwordVisible: boolean = false;
+
+  message: string = '';
+  isSuccess: boolean = false;
+  showMessage: boolean = false;
+
+  constructor(private router: Router) { }
+
+  togglePasswordVisibility() {
+    this.passwordVisible = !this.passwordVisible;
+  }
+
+  async login() {
+
+  }
+
+  irRegistro() {
+    this.router.navigate(['/register']);
+  }
+
+  volverInicio() {
+    this.router.navigate(['/']);
+  }
+
+  recuperarPassword() {
+    this.message = 'Función en desarrollo';
+    this.isSuccess = false;
+    this.showMessage = true;
+  }
 }
