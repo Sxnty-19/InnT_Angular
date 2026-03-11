130c6ebc858eac117ece7a7197abc816cb89a807
7DiegoVa7 <diegovasquezarteaga5@gmail.com>
2026-03-11 15:50:40 -0500

    Update  (No-Encontrado)

--- src/app/pages/no-encontrado/no-encontrado.ts
+++ src/app/pages/no-encontrado/no-encontrado.ts
@@ -1,11 +1,11 @@
+import { RouterLink } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-no-encontrado',
-  imports: [],
+  selector: 'app-not-found',
+  standalone: true,
+  imports: [RouterLink],  // ← esto es todo lo que falta
   templateUrl: './no-encontrado.html',
   styleUrl: './no-encontrado.css',
 })
-export class NoEncontrado {
-
-}
+export class NoEncontrado {}
